856e4c10a7ef5915bc9829ab90ac14b55ea656be
nondracek <nondracek@college.harvard.edu>
2018-02-08 17:00:58 -0500

    fixed py issue

--- P1-regression/FeatureEng.py
+++ P1-regression/FeatureEng.py
@@ -23,10 +23,15 @@ def getBin(smile):
     return list(map(int, fprint.ToBitString()))
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    Bits = executor.map(getBin, df_test['smiles'].iloc[1:100000], chunksize=100)
+    Bits_test = executor.map(getBin, df_test['smiles'].iloc[1:100], chunksize=10)
+    Bits_train = executor.map(getBin, df_train['smiles'].iloc[1:100], chunksize=10)
 
 print("Done with feature eng")
 
-with open("FingerPrintFeatures.csv", "wb") as f:
+with open("FingerPrintFeatures_train.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerows(Bits)
+    writer.writerows(Bits_train)
+
+with open("FingerPrintFeatures_test.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(Bits_test)
