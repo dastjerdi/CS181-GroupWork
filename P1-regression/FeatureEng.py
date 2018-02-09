cc55274f3549f42f14096598cde52840023a957f
nondracek <nondracek@college.harvard.edu>
2018-02-08 20:50:16 -0500

    fixed py issu3

--- P1-regression/FeatureEng.py
+++ P1-regression/FeatureEng.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
-import math
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import concurrent.futures
 import csv
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import PCA
+
 
 """
 Read in train and test as Pandas DataFrames
 """
+print("read data")
 df_train = pd.read_csv("train.csv")
 df_test = pd.read_csv("test.csv")
 
@@ -26,12 +30,31 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
     Bits_test = executor.map(getBin, df_test['smiles'], chunksize=100)
     Bits_train = executor.map(getBin, df_train['smiles'], chunksize=100)
 
-print("Done with feature eng")
 
-with open("FingerPrintFeatures_train.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(Bits_train)
+Y_train = df_train.gap.values
+X_train = pd.DataFrame(Bits_train)
+X_test = pd.DataFrame(Bits_test)
 
-with open("FingerPrintFeatures_test.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(Bits_test)
+print("Begin PCA")
+pca = PCA(n_components = 50)
+pca.fit(X_train, Y_train[1:100])
+
+X_PCA = pca.transform(X_train)
+
+X_PCA_Test = pca.transform(X_test)
+
+print("begin RF CV")
+forest = RandomForestRegressor()
+parameter = {'max_depth':[3,5,7], 'n_estimators':1024, }
+forest_cv = GridSearchCV(forest, parameter, cv = 3)
+forest_cv.fit(X_PCA, Y_train)
+
+print("RF CV done")
+def write_to_file(filename, predictions):
+    with open(filename, "w") as f:
+        f.write("Id,Prediction\n")
+        for i,p in enumerate(predictions):
+            f.write(str(i+1) + "," + str(p) + "\n")
+
+write_to_file("Preds.csv", forest_cv.predict(X_PCA_Test))
+print("File made!")
