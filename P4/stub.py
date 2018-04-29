118952b68a00eb1cf3a8b326bd0911342cf4d4d1
nondracek <nondracek@college.harvard.edu>
2018-04-29 19:56:34 -0400

    NN Working

--- P4/stub.py
+++ P4/stub.py
@@ -2,6 +2,7 @@
 import numpy as np
 import numpy.random as npr
 import pygame as pg
+import copy
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
@@ -18,48 +19,29 @@ class Learner(object):
 
     def __init__(self):
         self.last_state  = None
+        self.two_ago = None
         self.last_action = None
         self.last_reward = None
-        model = Sequential()
-        model.add(InputLayer(batch_input_shape=(1, 4)))
-        model.add(Dense(10, activation='sigmoid'))
-        model.add(Dense(2, activation='linear'))
-        model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-   
+        self.epsilon = .5
+        self.randomCount = 0
+        self.model = Sequential()
+        self.model.add(Dense(50, input_dim=4, kernel_initializer='normal', activation='relu'))
+        # self.model.add(Dense(10, activation='relu'))
+        self.model.add(Dense(2, activation='sigmoid'))
+        self.model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
 
     def reset(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
-    
+
     def state_RL(self, state):
         top_dist = state['tree']['top'] - state['monkey']['top']
         bot_dist = state['tree']['bot'] - state['monkey']['bot']
-        return (top_dist, bot_dist, state['tree']['dist'], state['monkey']['vel'])
+        new_state = np.array([[top_dist], [bot_dist], [state['tree']['dist']], [state['monkey']['vel']]])
 
-
-def eps_greedy_q_learning_with_table(env, num_episodes=500):
-    q_table = np.zeros((5, 2))
-    y = 0.95
-    eps = 0.5
-    lr = 0.8
-    decay_factor = 0.999
-    for i in range(num_episodes):
-        s = env.reset()
-        eps *= decay_factor
-        done = False
-        while not done:
-            # select the action with highest cummulative reward
-            if np.random.random() < eps or np.sum(q_table[s, :]) == 0:
-                a = np.random.randint(0, 2)
-            else:
-                a = np.argmax(q_table[s, :])
-            # pdb.set_trace()
-            new_s, r, done, _ = env.step(a)
-            q_table[s, a] += r + lr * (y * np.max(q_table[new_s, :]) - q_table[s, a])
-            s = new_s
-    return q_table
-
+        return new_state
 
 
 
@@ -75,24 +57,31 @@ def eps_greedy_q_learning_with_table(env, num_episodes=500):
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump.
 
-        RL_ state = state_RL(state)
-        
+        new_state = self.state_RL(state)
+        model = self.model
 
-        new_action = npr.rand() < 0.1
+        epsilon = self.epsilon*.9
+        self.epsilon = epsilon
 
-        a = np.argmax(model.predict(self.state_RL))
+        if np.random.rand() < epsilon:
+            self.randomCount += 1
+            a = np.random.randint(0,2)
+            print self.epsilon, self.randomCount
+        else:
+            a = np.argmax(model.predict(new_state.T))
 
 
-        self.last_action = new_action
-        new_state  = state
-        self.last_state  = new_state
-
+        self.last_action = a
+        self.two_ago = copy.copy(self.last_state)
+        self.last_state  = state
         return self.last_action
 
     def reward_callback(self, reward):
         last_reward = 0
         state = self.last_state
+        old_state = self.two_ago
 
+        ## Calculate Reward ##
         if state['monkey']['bot'] + state['monkey']['vel'] < 0 or state['monkey']['top'] + state['monkey']['vel'] > 400:
             last_reward = -10
 
@@ -103,6 +92,18 @@ def eps_greedy_q_learning_with_table(env, num_episodes=500):
                 last_reward = 1
 
         self.last_reward = reward
+
+        if old_state == None:
+            return
+
+        ## Update NN ##
+        model = self.model
+        old_state = self.state_RL(old_state).T
+        last_state = self.state_RL(state).T
+        Qvalues = model.predict(old_state)
+        Q_val = self.last_reward + .95*(np.max(model.predict(last_state)))
+        Qvalues[0][self.last_action] = Q_val
+        model.fit(old_state, Qvalues, epochs = 10, verbose = 1)
 
 
 def run_games(learner, hist, iters = 100, t_len = 100):
@@ -140,7 +141,7 @@ if __name__ == '__main__':
 	hist = []
 
 	# Run games.
-	run_games(agent, hist, 20, 10)
+	run_games(agent, hist, 100000, 1)
 
 	# Save history.
 	np.save('hist',np.array(hist))
