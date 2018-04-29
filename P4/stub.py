c12e766e940517ba5e7aed2f99fae8c1b7dca346
dastjerdi <dastjerdi@college.harvard.edu>
2018-04-29 17:31:54 -0400

    a'

--- P4/stub.py
+++ P4/stub.py
@@ -20,6 +20,37 @@ class Learner(object):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
+    
+    def state_RL(self):
+        state = self.last_state
+        top_dist = state['tree']['top'] - state['monkey']['top']
+        bot_dist = state['tree']['bot'] - state['monkey']['bot']
+        return (top_dist, bot_dist, state['tree']['dist'], state['monkey']['vel'])
+
+
+def eps_greedy_q_learning_with_table(env, num_episodes=500):
+    q_table = np.zeros((5, 2))
+    y = 0.95
+    eps = 0.5
+    lr = 0.8
+    decay_factor = 0.999
+    for i in range(num_episodes):
+        s = env.reset()
+        eps *= decay_factor
+        done = False
+        while not done:
+            # select the action with highest cummulative reward
+            if np.random.random() < eps or np.sum(q_table[s, :]) == 0:
+                a = np.random.randint(0, 2)
+            else:
+                a = np.argmax(q_table[s, :])
+            # pdb.set_trace()
+            new_s, r, done, _ = env.step(a)
+            q_table[s, a] += r + lr * (y * np.max(q_table[new_s, :]) - q_table[s, a])
+            s = new_s
+    return q_table
+
+
 
     def action_callback(self, state):
         '''
